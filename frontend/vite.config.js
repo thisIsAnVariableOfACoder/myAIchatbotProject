59c07604392384eafee755dfecc10d7aba42c96c
Nguyenpk <sensiblebran@gmail.com>
2026-02-07 04:26:37 +0700

    update web 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': 'http://localhost:3001'
-    }
-  }
-});
+  base: '/myAIchatbotProject/',
+})
